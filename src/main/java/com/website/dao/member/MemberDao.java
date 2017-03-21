241cc0e16984d4d145c9735140121addac8cb299
874341642 <874341642@qq.com>
2017-03-21 20:08:41 +0800

    初始化

---
+++ src/main/java/com/website/dao/member/MemberDao.java
@@ -0,0 +1,22 @@
+package com.website.dao.member;
+
+import java.util.List;
+
+import com.website.dao.base.BaseDao;
+import com.website.entity.member.MemberEntity;
+
+
+public interface MemberDao extends BaseDao {
+
+//	public MemberEntity getMember(String studentID);
+//
+//	public MemberEntity getStudentAndClass(String studentID);
+//
+//	public List<MemberEntity> getMemberAll();
+//
+//	public void insertStudent(MemberEntity entity);
+//
+//	public void deleteStudent(MemberEntity entity);
+//
+//	public void updateStudent(MemberEntity entity);
+}
