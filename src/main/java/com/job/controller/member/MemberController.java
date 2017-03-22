37fb2db674d50760f63e9f98bb721bd4675f6f33
874341642 <874341642@qq.com>
2017-03-22 22:07:26 +0800

    修改了controller

--- src/main/java/com/job/controller/member/MemberController.java
+++ src/main/java/com/job/controller/member/MemberController.java
@@ -30,9 +30,7 @@ public class MemberController {
 	{
 		Long id=1L;
 		String account=memberService.getMemberAccountById(id);
-		System.out.println(account);
-		System.out.println(account);
-		System.out.println(account);
+		System.out.println(account);	
 	}
 	
 	@ResponseBody
